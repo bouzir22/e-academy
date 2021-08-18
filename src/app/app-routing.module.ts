29a7ece823b4e1f0c341260cfb6f0c83df305fee
unknown <ahmedbouzir1995@gmail.com>
2021-08-18 23:15:35 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
+import { LandingComponent } from './components/landing/landing.component';
+import { CourseDetailsComponent } from './components/course-details/course-details.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'details', component: CourseDetailsComponent },
+  { path: '', component: LandingComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
